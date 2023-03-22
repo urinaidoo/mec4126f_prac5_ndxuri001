bda86cfaa75e94a91683f547ca9e005340aafe21
urinaidoo <111341234+urinaidoo@users.noreply.github.com>
2023-03-22 16:23:14 +0200

    Function definition code added
    Function definitions added as code for LCD, LED and interrupt configuration

--- main.c
+++ main.c
@@ -12,3 +12,61 @@ void init_LEDS(void);
 void display_on_LEDS(uint8_t i);
 void init_switches(void);
 void init_external_interrupts(void);
+
+
+void display_on_LCD(uint8_t i){
+
+
+	char str[3];
+	int deci = i;
+	sprintf(str,"%d", deci);
+	lcd_putstring(str);
+	delay(65000);
+	lcd_command(CLEAR);
+
+}
+
+void init_LEDS(void){
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	GPIOB->MODER |= GPIO_MODER_MODER0_0;
+	GPIOB->MODER |= GPIO_MODER_MODER1_0;
+	GPIOB->MODER |= GPIO_MODER_MODER2_0;
+	GPIOB->MODER |= GPIO_MODER_MODER3_0;
+	GPIOB->MODER |= GPIO_MODER_MODER4_0;
+	GPIOB->MODER |= GPIO_MODER_MODER5_0;
+	GPIOB->MODER |= GPIO_MODER_MODER6_0;
+	GPIOB->MODER |= GPIO_MODER_MODER7_0;
+}
+
+void display_on_LEDS(uint8_t i){
+	GPIOB->ODR = i;
+}
+
+
+void init_switches(void){
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER &= ~GPIO_MODER_MODER0;
+	GPIOA->MODER &= ~GPIO_MODER_MODER1;
+	GPIOA->MODER &= ~GPIO_MODER_MODER2;
+	GPIOA->MODER &= ~GPIO_MODER_MODER3;
+
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR0_0;
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR1_0;
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR2_0;
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR3_0;
+}
+
+void init_external_interrupts(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI3_PA;
+	EXTI->IMR |= EXTI_IMR_MR3;
+	EXTI->FTSR |= EXTI_FTSR_TR3;
+	NVIC_EnableIRQ(EXTI2_3_IRQn);
+}
+
+void EXTI2_3_IRQHandler(void)
+{
+	presscount+=1;
+	EXTI->PR |= EXTI_PR_PR3;
+}
